34eb004f75cb6b6e2bb89ecf50dda3fc3c5c7858
Prabin Parajuli <prabin.b9@gmail.com>
2021-01-27 17:53:51 +0100

    Attribute binding

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const app = Vue.createApp({
     // data, functions
     data() {
         return {
+            url: 'http://www.theninja.co.uk',
             showBooks: true,
             books: [
-                { title: 'name of the wind', author: 'patrick rothfuss' },
-                { title: 'the way of kings', author: 'brandon sanderson' },
-                { title: 'the final empire', author: 'brandon sand' }
+                { title: 'name of the wind', author: 'patrick rothfuss', img: '/assets/book1.jpg' },
+                { title: 'the way of kings', author: 'brandon sanderson', img: '/assets/book2.jpg' },
+                { title: 'the final empire', author: 'brandon sand', img: '/assets/book3.jpg' }
             ]
         }
     },
