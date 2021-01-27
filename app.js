79de01c9eac390e3477d44794640f297a1b87f43
Prabin Parajuli <prabin.b9@gmail.com>
2021-01-27 13:03:34 +0100

    Conditional Rendering. Toggle to show and hide.

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const app = Vue.createApp({
     // data, functions
     data() {
         return {
+            showBooks: true,
             title: 'The final Empire',
             author: 'Brandon Sanderson',
             age: 46
+        }
+    },
+    methods: {
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
         }
     }
 })
