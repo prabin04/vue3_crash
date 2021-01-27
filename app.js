ea8c35566b0b7fec99f2292e6fe308db537b0b8b
Prabin Parajuli <prabin.b9@gmail.com>
2021-01-27 18:10:44 +0100

    dynamic classes

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const app = Vue.createApp({
             url: 'http://www.theninja.co.uk',
             showBooks: true,
             books: [
-                { title: 'name of the wind', author: 'patrick rothfuss', img: '/assets/book1.jpg' },
-                { title: 'the way of kings', author: 'brandon sanderson', img: '/assets/book2.jpg' },
-                { title: 'the final empire', author: 'brandon sand', img: '/assets/book3.jpg' }
+                { title: 'name of the wind', author: 'patrick rothfuss', img: '/assets/book1.jpg', isFav: true },
+                { title: 'the way of kings', author: 'brandon sanderson', img: '/assets/book2.jpg', isFav: false },
+                { title: 'the final empire', author: 'brandon sand', img: '/assets/book3.jpg', isFav: true }
             ]
         }
     },
