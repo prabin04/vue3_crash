f094970a2c5bdd9dd4c5a488df4cbc82924e60de
Prabin Parajuli <prabin.b9@gmail.com>
2021-01-27 11:28:17 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    // data, functions
+    data() {
+        return {
+            title: 'The final Empire',
+            author: 'Brandon Sanderson',
+            age: 46
+        }
+    }
+})
+
+app.mount("#app")
