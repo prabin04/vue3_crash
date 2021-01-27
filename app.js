33c6eb1c7715f4e2e3dfd8ea41086e1742d13312
Prabin Parajuli <prabin.b9@gmail.com>
2021-01-27 14:08:42 +0100

    mouse events

--- app.js
+++ app.js
@@ -5,12 +5,24 @@ const app = Vue.createApp({
             showBooks: true,
             title: 'The final Empire',
             author: 'Brandon Sanderson',
-            age: 46
+            age: 46,
+            x: 0,
+            y: 0
         }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks
+        },
+        handleEvent(e, data) {
+            console.log(e, e.type)
+            if (data) {
+                console.log(data)
+            }
+        },
+        handleMousemove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 })
